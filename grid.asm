8fda39a91e95692c8a42b99a9186284431f0dbf1
Sol Boucher <slb1566@rit.edu>
2013-02-03 04:35:28 -0500

    Implemented setting *row*s' expected tree counts

--- grid.asm
+++ grid.asm
@@ -3,6 +3,9 @@
 # @author Sol Boucher <slb1566@rit.edu>
 
 .globl	init_grid
+.globl	len_grid
+.globl	set_expected
+.globl	set_expected
 
 # The minimum edge length of the square grid.
 MIN_LEN = 2
@@ -42,10 +45,63 @@ init_grid:
 	#	return 0
 	sge	$t0,$a0,MIN_LEN
 	sle	$v0,$a0,MAX_LEN
-	and	$v0,$t0,$v0
+	and	$v0,$t0,$v0 #must be within both bounds
 	beq	$v0,$zero,bad_len
 		la	$t0,len
 		sw	$a0,0($t0)
 	bad_len:
 	
+	jr	$ra
+
+# Length.
+# @v 0 side length of this grid
+len_grid:
+	la	$t0,len
+	lw	$v0,0($t0)
+	jr	$ra
+
+# @a 0 0 for row, 1 for column
+# @a 1 row or column of interest
+# @a 2 expected number of tents in that group
+# @v 0 1 for success, 1 for unrealistic expectation
+set_expected:
+	sge	$t0,$a2,$zero
+	
+	#check against length as upper bound:
+	la	$t1,len
+	lw	$t1,0($t1)
+	sle	$v0,$a2,$t1
+	
+	and	$v0,$t0,$v0 #must be within both bounds
+	beq	$v0,$zero,bad_group
+		#get the address of the row or col expectations:
+		bne	$a0,$zero,exscol
+			la	$t0,tentsr #using rows
+			j	exspicked
+		exscol:	la	$t0,tentsc #using cols
+		exspicked:
+		
+		#store the given value in the correct place:
+		mul	$a1,$a1,4
+		add	$t0,$t0,$a1
+		sw	$a2,0($t0)
+	bad_group:
+	
+	jr	$ra
+
+# @a 0 0 for row, 1 for column
+# @a 1 row of interest
+# @v 0 expected number of tents in that row
+get_expected:
+	#get the address of the row or col expectations:
+	bne	$a0,$zero,exgcol
+		la	$t0,tentsr #using rows
+		j	exgpicked
+	exgcol:	la	$t0,tentsc #using cols
+	exgpicked:
+	
+	#load the value from the provided location:
+	mul	$a1,$a1,4
+	add	$t0,$t0,$a1
+	lw	$v0,0($t0)
 	jr	$ra
--- tents.asm
+++ tents.asm
@@ -4,6 +4,9 @@
 
 .globl	main
 .globl	init_grid
+.globl	len_grid
+.globl	set_expected
+.globl	get_expected
 
 # The system call for reading an int.
 READ_INT = 5
@@ -14,9 +17,11 @@ PRINT_STR = 4
 .data
 .align 2
 
-# The feedback for invalid sizes.
+# The feedback to give on errors:
 err_1:
 	.asciiz	"Invalid board size, Tents terminating\n"
+err_2:
+	.asciiz	"Illegal sum value, Tents terminating\n"
 
 .text
 .align 2
@@ -31,12 +36,41 @@ main:
 	move	$a0,$v0
 	jal	init_grid
 	lw	$ra,0($sp)
-	bne	$v0,$zero,ok_size
+	bne	$v0,$zero,ok_size #if out of range
 		li	$v0,PRINT_STR
 		la	$a0,err_1
 		syscall
-		jr	$ra
+		jr	$ra #bail out!
 	ok_size:
+	
+	#record the expected trees per row
+	jal	len_grid
+	lw	$ra,0($sp)
+	move	$s0,$v0		#number of rows/cols
+	move	$s1,$zero	#working with rows (not cols)
+	move	$s2,$zero	#current index
+	exp_r: #for each reported row
+		#use current grouping scheme and index
+		move	$a0,$s1
+		move	$a1,$s2
+		
+		#use user-provided expected count:
+		li	$v0,READ_INT
+		syscall
+		move	$a2,$v0
+		
+		#give setting that a try
+		jal	set_expected
+		lw	$ra,0($sp)
+		bne	$v0,$zero,ok_expectation #if unreasonable
+			li	$v0,PRINT_STR
+			la	$a0,err_2
+			syscall
+			jr	$ra #bail out!
+		ok_expectation:
+		
+		addi	$s2,$s2,1
+		bne	$s2,$s0,exp_r #until out of rows
 	
 	lw	$ra,0($sp)
 	addi	$sp,$sp,4
